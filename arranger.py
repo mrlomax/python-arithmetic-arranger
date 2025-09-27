3eb971a4f95375e9eed3029a694eca26b338790f
mrlomax <mattylomax@googlemail.com>
2025-09-27 14:41:08 +0100

    feat: Right align arithmetic formatter function

--- arranger.py
+++ arranger.py
@@ -1,32 +1,50 @@
 def arithmetic_arranger(problems, show_answers=False):
-    total_problems = 0
+    first_row_list = []
+    second_row_list = []
+    third_row_list = []
+    fourth_row_list = []
+    output = []
 
-    for problem in problems:
+    if len(problems) > 5:
+        return('Error: Too many problems.')
+
+    for i, problem in enumerate(problems):
         parts = problem.split()
-        first_number, operator, second_number = parts #int(parts[0]), parts[1], int(parts[2])
-        total_problems += 1
+        first_number, operator, second_number = parts
 
-        #print(parts, total_problems)
-
-        if total_problems > 5:
-            raise ValueError('Error: Too many problems.')
-        elif operator not in ('+', '-'):
-            raise ValueError("Error: Operator must be '+' or '-'.")
+        if operator not in ('+', '-'):
+            return("Error: Operator must be '+' or '-'.")
+        elif not first_number.isdigit() or not second_number.isdigit():
+            return('Error: Numbers must only contain digits.')
         elif len(first_number) > 4 or len(second_number) > 4:
-            raise ValueError('Error: Numbers cannot be more than four digits.')
+            return('Error: Numbers cannot be more than four digits.')
         else:
-            print_list = []
-            first_pad = 4 - len(first_number)
-            second_pad = 4 - len(second_number)
-            print_list = ([' '] * first_pad) + list(first_number) + list('\n') + ([' '] * second_pad) + list(second_number) + list('\n') + list('----')
+            pad = max(len(first_number), len(second_number)) + 2
+            first_row_list.extend(([' '] * (pad - len(first_number))) + list(first_number))
+            second_row_list.extend(list(operator) + ([' '] * (pad - len(second_number) - 1)) + list(second_number))
+            third_row_list.extend((['-'] * pad))
 
-            print(''.join(print_list))
-        
+            if operator == '+':
+                answer = int(first_number) + int(second_number)
+            elif operator == '-':
+                answer = int(first_number) - int(second_number)
+            
+            fourth_row_list.extend(([' '] * (pad - len(str(answer))) + list(str(answer))))
 
-    return problems
+            if i < len(problems):
+                first_row_list.extend('    ')
+                second_row_list.extend('    ')
+                third_row_list.extend('    ')
+                fourth_row_list.extend('    ')
+
+    output.extend(first_row_list + list('\n') + second_row_list + list('\n') + third_row_list)
+    if show_answers == True:
+        output.extend(list('\n') + fourth_row_list)
+
+    return ''.join(output)
 
 def main ():
-    print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+    print(f'\n{arithmetic_arranger(["3801 - 2", "123 + 49"])}')
 
 if __name__ == '__main__':
     main()
